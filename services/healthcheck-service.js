d721c22d45f2de2fc0372f7d3bc0f60ad8b385bc
Paulo Barros <paulo.alfredo.barros@gmail.com>
2020-02-11 16:14:28 -0300

    feat: Add logger

--- services/healthcheck-service.js
+++ services/healthcheck-service.js
@@ -1,5 +1,11 @@
+const logger = require('../clients/logger')
+
 const HealthCheckService = {
   async status() {
+    logger.debug({
+      action: 'healthcheckService.status',
+      msg: 'Check API health'
+    })
     return Promise.resolve({ status: 'ok' })
   }
 }
